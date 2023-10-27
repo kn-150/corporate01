667d7421f834e1a9d628f8c186d1df70dc2829ed
yamamura <tonyhawkingsexample@example.com>
2023-10-27 22:24:30 +0900

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,277 @@
+/* top ===============================*/
+window.addEventListener("scroll", function () {
+  // アニメーションを実行したいエレメントを取得
+  const elements = document.getElementsByClassName(
+    "top_companyMissionContainer"
+  );
+
+  // 複数のエレメントに対して、アニメーションを実行する
+  Array.prototype.forEach.call(elements, function (element) {
+    // ウィンドウの上端とエレメントの上端の距離を取得
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // アニメーションの条件をチェック
+    if (elementTop - windowHeight <= 0) {
+      // アニメーションを実行するためのコードをここに記述
+      element.classList.add("fadeUp");
+    }
+  });
+});
+
+window.addEventListener("scroll", function () {
+  // アニメーションを実行したいエレメントを取得
+  const elements = document.getElementsByClassName("top_businessContainer");
+
+  // 複数のエレメントに対して、アニメーションを実行する
+  Array.prototype.forEach.call(elements, function (element) {
+    // ウィンドウの上端とエレメントの上端の距離を取得
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // アニメーションの条件をチェック
+    if (elementTop - windowHeight <= 0) {
+      // アニメーションを実行するためのコードをここに記述
+      element.classList.add("fadeUp");
+    }
+  });
+});
+
+window.addEventListener("scroll", function () {
+  // アニメーションを実行したいエレメントを取得
+  const elements = document.getElementsByClassName("top_companyInfoContainer");
+
+  // 複数のエレメントに対して、アニメーションを実行する
+  Array.prototype.forEach.call(elements, function (element) {
+    // ウィンドウの上端とエレメントの上端の距離を取得
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // アニメーションの条件をチェック
+    if (elementTop - windowHeight <= 0) {
+      // アニメーションを実行するためのコードをここに記述
+      element.classList.add("fadeUp");
+    }
+  });
+});
+
+window.addEventListener("scroll", function () {
+  // アニメーションを実行したいエレメントを取得
+  const elements = document.getElementsByClassName("top_partnerContainer");
+
+  // 複数のエレメントに対して、アニメーションを実行する
+  Array.prototype.forEach.call(elements, function (element) {
+    // ウィンドウの上端とエレメントの上端の距離を取得
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // アニメーションの条件をチェック
+    if (elementTop - windowHeight <= 0) {
+      // アニメーションを実行するためのコードをここに記述
+      element.classList.add("fadeUp");
+    }
+  });
+});
+
+window.addEventListener("scroll", function () {
+  // アニメーションを実行したいエレメントを取得
+  const elements = document.getElementsByClassName("top_othersContainer");
+
+  // 複数のエレメントに対して、アニメーションを実行する
+  Array.prototype.forEach.call(elements, function (element) {
+    // ウィンドウの上端とエレメントの上端の距離を取得
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // アニメーションの条件をチェック
+    if (elementTop - windowHeight <= 0) {
+      // アニメーションを実行するためのコードをここに記述
+      element.classList.add("fadeUp");
+    }
+  });
+});
+
+
+/* purpose ===============================*/
+window.addEventListener("scroll", function () {
+  // アニメーションを実行したいエレメントを取得
+  const elements = document.getElementsByClassName("purpose_topTitle");
+
+  // 複数のエレメントに対して、アニメーションを実行する
+  Array.prototype.forEach.call(elements, function (element) {
+    // ウィンドウの上端とエレメントの上端の距離を取得
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // アニメーションの条件をチェック
+    if (elementTop - windowHeight <= 0) {
+      // アニメーションを実行するためのコードをここに記述
+      element.classList.add("fadeUp");
+    }
+  });
+});
+
+window.addEventListener("scroll", function () {
+  // アニメーションを実行したいエレメントを取得
+  const elements = document.getElementsByClassName("purpose_topText");
+
+  // 複数のエレメントに対して、アニメーションを実行する
+  Array.prototype.forEach.call(elements, function (element) {
+    // ウィンドウの上端とエレメントの上端の距離を取得
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // アニメーションの条件をチェック
+    if (elementTop - windowHeight <= 0) {
+      // アニメーションを実行するためのコードをここに記述
+      element.classList.add("fadeUp");
+    }
+  });
+});
+
+window.addEventListener("scroll", function () {
+  // アニメーションを実行したいエレメントを取得
+  const elements = document.getElementsByClassName(
+    "purpose_boardMemberItemRight"
+  );
+
+  // 複数のエレメントに対して、アニメーションを実行する
+  Array.prototype.forEach.call(elements, function (element) {
+    // ウィンドウの上端とエレメントの上端の距離を取得
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // アニメーションの条件をチェック
+    if (elementTop - windowHeight <= 0) {
+      // アニメーションを実行するためのコードをここに記述
+      element.classList.add("fadeUp");
+    }
+  });
+});
+
+window.addEventListener("scroll", function () {
+  // アニメーションを実行したいエレメントを取得
+  const elements = document.getElementsByClassName(
+    "purpose_boardMemberItemLeft"
+  );
+
+  // 複数のエレメントに対して、アニメーションを実行する
+  Array.prototype.forEach.call(elements, function (element) {
+    // ウィンドウの上端とエレメントの上端の距離を取得
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // アニメーションの条件をチェック
+    if (elementTop - windowHeight <= 0) {
+      // アニメーションを実行するためのコードをここに記述
+      element.classList.add("fadeUp");
+    }
+  });
+});
+
+
+/* business-content ===============================*/
+window.addEventListener("scroll", function () {
+  // アニメーションを実行したいエレメントを取得
+  const elements = document.getElementsByClassName(
+    "businessContent_backgroundFirst"
+  );
+
+  // 複数のエレメントに対して、アニメーションを実行する
+  Array.prototype.forEach.call(elements, function (element) {
+    // ウィンドウの上端とエレメントの上端の距離を取得
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // アニメーションの条件をチェック
+    if (elementTop - windowHeight <= 0) {
+      // アニメーションを実行するためのコードをここに記述
+      element.classList.add("fadeUp");
+    }
+  });
+});
+
+window.addEventListener("scroll", function () {
+  // アニメーションを実行したいエレメントを取得
+  const elements = document.getElementsByClassName(
+    "businessContent_contentItemSecond"
+  );
+
+  // 複数のエレメントに対して、アニメーションを実行する
+  Array.prototype.forEach.call(elements, function (element) {
+    // ウィンドウの上端とエレメントの上端の距離を取得
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // アニメーションの条件をチェック
+    if (elementTop - windowHeight <= 0) {
+      // アニメーションを実行するためのコードをここに記述
+      element.classList.add("fadeUp");
+    }
+  });
+});
+
+window.addEventListener("scroll", function () {
+  // アニメーションを実行したいエレメントを取得
+  const elements = document.getElementsByClassName(
+    "businessContent_contentItemThird"
+  );
+
+  // 複数のエレメントに対して、アニメーションを実行する
+  Array.prototype.forEach.call(elements, function (element) {
+    // ウィンドウの上端とエレメントの上端の距離を取得
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // アニメーションの条件をチェック
+    if (elementTop - windowHeight <= 0) {
+      // アニメーションを実行するためのコードをここに記述
+      element.classList.add("fadeUp");
+    }
+  });
+});
+
+window.addEventListener("scroll", function () {
+  // アニメーションを実行したいエレメントを取得
+  const elements = document.getElementsByClassName(
+    "businessContent_contentItemFourth"
+  );
+
+  // 複数のエレメントに対して、アニメーションを実行する
+  Array.prototype.forEach.call(elements, function (element) {
+    // ウィンドウの上端とエレメントの上端の距離を取得
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    // アニメーションの条件をチェック
+    if (elementTop - windowHeight <= 0) {
+      // アニメーションを実行するためのコードをここに記述
+      element.classList.add("fadeUp");
+    }
+  });
+});
+
+// ハンバーガーメニューボタン
+$(document).ready(function () {
+  $(".open-btn").click(function () {
+    $(this).toggleClass('active');
+    $(".header_nav").toggleClass('panel-active');//ナビゲーションにpanel-activeクラスを付与
+  });
+});
+
+$(document).ready(function () {
+  $(".header_nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".open-btn").removeClass('active');//ボタンの activeクラスを除去し
+    $("header_nav").removeClass('panel-active');//ナビゲーションのpanel-activeクラスも除去
+  });
+});
+
+$(function(){
+  $('.check').on('click', function() {
+    if ($(this).prop('checked')){
+      // 一旦全てをクリアして再チェックする
+      $('.check').prop('checked', false);
+      $(this).prop('checked', true);
+    }
+  });
+});
